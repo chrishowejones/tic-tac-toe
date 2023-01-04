eed53a09ef474b485ce5584c53b7d01c5bffe9a6
Chris Howe-Jones <chris.howejones@gmail.com>
2023-01-04 09:57:50 +0000

    Added tests

--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -1,7 +1,57 @@
 (ns tic-tac-toe.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [tic-tac-toe.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest winning-line-found
+  (is (= :x (winning-line [:x :x :x])))
+  (is (= :o (winning-line [:o :o :o]))))
+
+(deftest draw-found
+  (is (= :draw (winning-line [:x :o :x])))
+  (is (= :draw (winning-line [:o :o :x]))))
+
+(deftest incomplete-line
+  (is (= :ongoing (winning-line [nil :o :x])))
+  (is (= :ongoing (winning-line [nil nil nil]))))
+
+(def incomplete-board
+  [[:o nil nil]
+   [:x nil nil]
+   [:o nil nil]])
+
+(deftest ongoing-game
+  (is (= :ongoing (analyze incomplete-board))))
+
+(def drawn-board
+  [[:o :x :x]
+   [:x :o :o]
+   [:o :x :x]])
+
+(deftest drawn-game
+  (is (= :draw (analyze drawn-board))))
+
+(def x-win-horizontal
+  [[:o :x :o]
+   [:x :x :x]
+   [:x :o :o]])
+
+(def x-win-vertical
+  [[:o :x :x]
+   [:x :x :o]
+   [:o :x :o]])
+
+(def x-win-diagonal-back
+  [[:o :o :x]
+   [:x :x :o]
+   [:x :x :o]])
+
+(def x-win-diagonal-forward
+  [[:x :o :o]
+   [:x :x :o]
+   [:o :x :x]])
+
+(deftest x-wins
+  (is (= :x (analyze x-win-vertical)))
+  (is (= :x (analyze x-win-horizontal)))
+  (is (= :x (analyze x-win-diagonal-back)))
+  (is (= :x (analyze x-win-diagonal-forward))))
